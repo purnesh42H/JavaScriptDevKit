2da85ecb2d95dea6fab27a1be6e2f98784b614ab
purnesh42h <purnesh.dixit92@gmail.com>
2017-05-22 16:14:03 -0400

    Testing and CI

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,7 @@
-var chalk = require('chalk')
+//var chalk_std = require('chalk'); //standard js
 
-console.log(chalk.green('Starting app in dev mode...'));
+import chalk_babel from 'chalk'; //babel js which transpiles to standard  js
+
+console.log(chalk_babel.green('Starting app in dev mode...'));
+// Note: To run babel script i.e. ES6 use babel-node instead of just node
+// For example, check scripts part of package.json
