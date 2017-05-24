99d91215a9bc69fbc719877c4b54a9af2034449d
purnesh42h <purnesh.dixit92@gmail.com>
2017-05-23 22:15:57 -0400

    Automatic Production Deployment

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+
 //var chalk_std = require('chalk'); //standard js
 
 import chalk_babel from 'chalk'; //babel js which transpiles to standard  js
--- src/index.test.js
+++ src/index.test.js
@@ -2,18 +2,12 @@ import {expect} from 'chai';
 import fs from 'fs';
 import jsdom from 'jsdom';
 
-describe('Our first test', () => {
-    it('should pass', () => {
-        expect(true).to.equal(true);
-    });
-});
-
 describe('index.html', () => {
-    it('Should say hello', (done) => {
+    it('Should h1 that says Users', (done) => {
         const index = fs.readFileSync('./src/index.html', "utf-8");
         jsdom.env(index, function (err, window) {
             const h1 = window.document.getElementsByTagName('h1')[0]
-            expect(h1.innerHTML).to.equal("Best JS Kit Ever!");
+            expect(h1.innerHTML).to.equal("Users");
             done();
             window.close();
         });
